29732decee0a3a9d2defdcec0bed83a691b040d3
thddmsdn <56345564+thddmsdn@users.noreply.github.com>
2022-08-02 18:01:09 +0900

    Update index.js

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://127.0.0.1:8090/',
+	baseURL: 'https://purple-dune-046dccb00.1.azurestaticapps.net/',
 })
 
 instance.defaults.headers.post['Content-Type'] =
