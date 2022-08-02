6b30f95bd25d449cf812c21a36633cde82c0339f
thddmsdn <56345564+thddmsdn@users.noreply.github.com>
2022-08-02 15:55:39 +0900

    Update vue.config.js

--- board-app/vue.config.js
+++ board-app/vue.config.js
@@ -1,15 +1,12 @@
 module.exports = {
 	transpileDependencies: ['vuetify'],
 	chainWebpack: config => {
+		config.module.rules.delete('eslint');
 		config.plugin('html').tap(args => {
 			args[0].title = 'Board'
 			return args
 			
-//module.exports = {
-//   chainWebpack: config => {
-//        config.module.rules.delete('eslint');
-//    }
-//}			
+			
 		})
 	},
 	devServer: {
