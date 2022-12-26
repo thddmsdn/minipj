ee85d42cf90036e3663bf10ec0d7eb5dab6f4e34
thddmsdn <56345564+thddmsdn@users.noreply.github.com>
2022-12-26 18:40:20 +0900

    Update index.js

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://20.196.253.201:8090/',
+	baseURL: 'http://20.200.207.174:8090/',
 })
 
 instance.defaults.headers.post['Content-Type'] =
